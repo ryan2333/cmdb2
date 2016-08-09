2450b481c482e84149960151c012019c393d5eb9
rzhao2333 <543551194@qq.com>
2016-08-09 18:02:59 +0800

    user modules finished!

--- user/models.py
+++ user/models.py
@@ -2,17 +2,94 @@
 # -*- coding: utf-8 -*-
 from dbutil import MySQLConnection
 
+class UserType(object):
+	@classmethod
+	def get_user_types(cls):
+		_sql = 'select tid, tname from user_types'
+		_count, _rt_list = MySQLConnection.execute_sql(_sql,())
+		return _rt_list
+
+
 
 class User(object):
 	table = 'users'
 	key = 'id'
-	columns_select = ['id', 'username', 'password', 'email']
+	columns_select = ['uid', 'username', 'password', 'email']
 	columns_add = ['username', 'password', 'email']
 	columns_update = ['email']
 
 	@classmethod
 	def validate_login(cls, username, password):
-		_columns = ('id', 'username')
-		_sql = 'select id, username from users where username=%s and password=md5(%s)'
+		_columns = ('uid', 'username')
+		_sql = 'select uid, username from users where username=%s and password=md5(%s)'
 		_count, _rt_list = MySQLConnection.execute_sql(_sql, (username, password))
 		return dict(zip(_columns, _rt_list[0])) if _count != 0 else None
+
+	@classmethod
+	def get_users(cls):
+		#_sql = 'select * from users where status=0'
+		_sql = 'select users.uid, users.username,users.password, users.email, user_types.tname as user_type  from users inner join user_types on users.user_type = user_types.tid where status=0;'
+		_columns = ('uid', 'username', 'password', 'email', 'user_type')
+		_count, _rt_list = MySQLConnection.execute_sql(_sql,())
+		userlist = []
+		for i in _rt_list:
+			userlist.append(dict(zip(_columns, i)))
+		return userlist
+
+
+	@classmethod
+	def get_user_by(cls,k,v):
+		_sql = 'select * from users where status=0 and {k}=%s'.format(k=k)
+		args=(v,)
+		_columns = ('uid', 'username', 'password', 'email', 'user_type')
+		_count, _rt_list = MySQLConnection.execute_sql(_sql, args)
+		return dict(zip(_columns, _rt_list[0])) if _count != 0 else None
+
+
+	@classmethod
+	def validate_user_add(cls,username, password, reppassword):
+		errors = []
+		if len(username) < 6:
+			errors.append('用户名长度不能小于6')
+		if password != reppassword:
+			errors.append('两次密码输入不一致')
+		if len(password) <8:
+		 	errors.append('密码长度不能小于8')
+		_rt_list = cls.get_user_by('username', username)
+		if _rt_list:
+			errors.append('用户名已存在')
+		if errors:
+			return False, '\n'.join(errors)
+		else:
+			return True, '' 
+
+	@classmethod
+	def user_add(cls, username,password, email, user_type):
+		_sql = 'insert into users(username,password,email,user_type) values(%s, md5(%s),%s,%s)'
+		_count, _rt_list = MySQLConnection.execute_sql(_sql,(username,password, email, user_type), False)
+		if _count:
+			return True, ''
+		else:
+			return False, ''
+
+
+	@classmethod
+	def user_update(cls, uid, username, email, user_type):
+		_sql = 'update users set email=%s, user_type=%s where uid=%s and username=%s'
+		args = (email, user_type, uid, username)
+		_count, _rt_list = MySQLConnection.execute_sql(_sql,args, False)
+		if _count:
+			return True, ''
+		else:
+			return False, ''
+
+	@classmethod
+	def user_delete(cls, uid,username):
+		_sql = 'delete from users where uid=%s and username=%s'
+		args = (uid,username)
+		_count, _rt_list = MySQLConnection.execute_sql(_sql, args, False)
+
+		if _count:
+			return True, ''
+		else:
+			return False, ''
--- user/views.py
+++ user/views.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8') 
 
 from . import app
-from flask import render_template, request, redirect
+from functools import wraps
+from flask import render_template, request, redirect, session, flash
+from models import User,UserType
+import json
 
 def login_required(func):
 
@@ -22,11 +28,97 @@ def index():
     return render_template('login.html')
 
 
+def userType():
+    user_type=UserType.get_user_types()
+    print user_type
+    return user_type
+
+
+@login_required
+@app.route('/users/')
+def user_list():
+    _user = User.get_users()
+    return render_template('users.html', users=_user)
+
+
 @app.route('/login/', methods=['post', 'get'])
 def login():
-	username = request.form.get('username', '')
-	password = request.form.get('password', '')
+    username = request.form.get('username', '')
+    password = request.form.get('password', '')
 
+    _user = User.validate_login(username, password)
+    if _user:
+        session['user'] = _user
+        return redirect('/users/')
+    else:
+        return render_template('login.html', username=username, error='用户名或密码错误')
+
+
+@login_required
+@app.route('/user/create/')
+def user_create():
+    return render_template('user_create.html', userType=userType())
+
+
+@login_required
+@app.route('/user/add/', methods=['post'])
+def user_add():
+    username = request.form.get('username', '')
+    password = request.form.get('password', '')
+    reppassword = request.form.get('passwordrep', '')
+    email = request.form.get('email', '')
+    usertype = request.form.get('usertype', 0)
+
+    _is_ok, _errors = User.validate_user_add(username,password,reppassword)
+
+    if _is_ok:
+        User.user_add(username,password,email,usertype)
+        return json.dumps({'is_ok': _is_ok, 'success': '用户创建成功','error':_errors})
+    else:
+        return json.dumps({'is_ok': _is_ok, 'error':_errors})
+
+
+@login_required
+@app.route('/user/modify/')
+def user_modify():
+    uid = request.args.get('uid', '')
+    _user = User.get_user_by('uid', uid)
+    return render_template('user_modify.html', users=_user, usertype=userType())
+
+
+@login_required
+@app.route('/user/update/', methods=['post'])
+def user_update():
+    uid = request.form.get('userid', '')
+    username = request.form.get('username', '')
+    email = request.form.get('email', '')
+    usertype = request.form.get('usertype', 0)
+    _is_ok, errors = User.user_update(uid, username, email, usertype)
+    if _is_ok:
+        return json.dumps({'is_ok':_is_ok, 'success':'用户信息更新成功'})
+    else:
+        return json.dumps({'is_ok':_is_ok, 'error':'用户信息更新失败'})
+
+
+@login_required
+@app.route('/user/delete/')
+def user_delete():
+    uid = request.args.get('uid','')
+    username = request.args.get('username','')
+    _is_ok, errors = User.user_delete(uid, username)
+    if _is_ok:
+        flash('用户删除成功')
+        return redirect('/users/')
+    else:
+        flash('用户删除失败')
+        return redirect('/users/')
+
+
+@login_required
+@app.route('/logout/')
+def logout():
+    session.clear()
+    return redirect('/')
 
 if __name__ == '__main__':
 	import utils
